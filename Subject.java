847633ec47236e7912c6b7f392bffb528552cb0f
Chin Hongnyheng <hongnyhengchin@gmail.com>
2025-01-08 15:44:20 +0700

    Updated

--- Subject.java
+++ Subject.java
@@ -1,6 +1,44 @@
-class Subject {
+public class Subject {
+    private String name;
+    private String code;
+    private int creditHours;
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public Subject(String name, String code, int creditHours) {
+        this.name = name;
+        this.code = code;
+        this.creditHours = creditHours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                ", creditHours=" + creditHours +
+                '}';
     }
 }
